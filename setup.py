f09e801c2b08216b5ae507f5d7046fe7d1766c3a
Sanjam Preet <sanjam.preet@gmail.com>
2025-07-24 15:10:50 -0400

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,38 @@
+'''
+The setup.py file is an essential part of packaging and
+distributing Python projects. It is used by setuptools 
+(or distutils in older Python versions) to define the configuration 
+of your project such as its metadata, dependencies and more.  
+'''
+
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements()-›List[str]
+    '''
+    This function will return the list of requirements
+    '''
+    requirement_lst:List[str]=[]
+    try:
+        with open ('requirements.txt', 'r') as file:
+            #Read lines from the file 
+            lines=file.readlines()
+            ## Process each line 
+            for line in lines:
+                requirement=line.strip()
+                # Ignore empty lines and -e 
+                if requirement and requirement!= '-e .':
+                    requirement_lst.append (requirement)
+    except FileNotFoundError:
+        print("requirements. txt file not found")
+
+    return requirement_lst
+
+setup(
+    name="Sample Project",
+    version="0.0.1",
+    author="Sanjam Preet",
+    author_email="sanjam.preet@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
